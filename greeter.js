6df815d2d88f3ebb5f414708030accde9a9438b9
NathanielYoung <young.nathaniel95@gmail.com>
2018-10-26 21:27:45 -0600

    TypeScript

---
+++ greeter.js
@@ -0,0 +1,15 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
+// converted this file to a greeter.js by typing tsc greeter.ts in command line
---
+++ greeter.ts
@@ -0,0 +1,22 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
+
+
+// converted this file to a greeter.js by typing tsc greeter.ts in command line
